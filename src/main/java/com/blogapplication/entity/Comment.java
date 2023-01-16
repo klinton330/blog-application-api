ce7d0fb9722a05f4cb31aaec246303893ffbbe58
HOME <klintonece@gmail.com>
2023-01-17 01:02:15 +0530

    Spring Boot 3.0 Migration

--- src/main/java/com/blogapplication/entity/Comment.java
+++ src/main/java/com/blogapplication/entity/Comment.java
@@ -3,7 +3,7 @@ package com.blogapplication.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Setter
 @Getter
